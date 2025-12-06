151808204c28fe4d3bb3f0c1f8ff5f6626a18f70
felipeasoares9 <133257981+felipeasoares9@users.noreply.github.com>
2025-12-06 13:49:12 -0300

    Update ClientesController.cs

--- LojaCarros/Controllers/ClientesController.cs
+++ LojaCarros/Controllers/ClientesController.cs
@@ -50,8 +50,6 @@ namespace LojaCarros.Controllers
         }
 
         // POST: Clientes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Nome,DataNasc,Email,Telefone,Endereco,Cpf")] Cliente cliente)
@@ -82,8 +80,6 @@ namespace LojaCarros.Controllers
         }
 
         // POST: Clientes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,DataNasc,Email,Telefone,Endereco,Cpf")] Cliente cliente)
